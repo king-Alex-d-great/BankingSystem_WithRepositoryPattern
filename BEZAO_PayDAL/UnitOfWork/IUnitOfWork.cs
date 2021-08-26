24614616a1b58aab33e427ff83d5d9a770c04794
ogubuike Alex <ogubuikealex@gmail.com>
2021-08-26 09:02:58 +0100

    Added unit of work

---
+++ BEZAO_PayDAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BEZAO_PayDAL.Entities;
+using BEZAO_PayDAL.Interfaces;
+
+namespace BEZAO_PayDAL.UnitOfWork
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepository<Transaction> Transactions { get; }
+        IRepository<Account> Accounts { get; }
+        IRepository<User> Users { get; }
+        void commit();       
+    }
+}
---
+++ BEZAO_PayDAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BEZAO_PayDAL.Entities;
+using BEZAO_PayDAL.Interfaces;
+using BEZAO_PayDAL.Repositories;
+
+namespace BEZAO_PayDAL.UnitOfWork
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private BezaoPayContext _context;
+
+        private IRepository<Transaction> _transactions;
+        private IRepository<Account> _accounts;
+        private IRepository<User> _users;
+
+        public UnitOfWork(BezaoPayContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Transaction> Transactions { get { return _transactions ??= (_transactions = new Repository<Transaction>(_context)); } } 
+        public IRepository<Account> Accounts { get { return _accounts ??= (_accounts = new Repository<Account>(_context)); } } 
+        public IRepository<User> Users { get { return _users ??= (_users = new Repository<User>(_context)); } }
+       
+        public void commit()
+        {
+            _context.SaveChanges();
+        }        
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
